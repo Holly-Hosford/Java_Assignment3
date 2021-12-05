a3c1fb6ad9f1b2455f419b333e5e4f76f97ed3d8
Holly Hosford <119407142@umail.ucc.ie>
2021-12-05 23:13:00 +0000

    Changed Apllication.java to App.java as problems were occuring running the application

--- Java_Assignment3/src/main/java/com/example/java_assignment3/App.java
+++ Java_Assignment3/src/main/java/com/example/java_assignment3/App.java
@@ -1,15 +1,16 @@
 package com.example.java_assignment3;
 
+import javafx.fxml.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Application extends javafx.application.Application {
+public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Registration Form");
         stage.setScene(scene);
