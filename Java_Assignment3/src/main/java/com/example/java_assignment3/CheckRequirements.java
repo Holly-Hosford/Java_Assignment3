ad206b7a22fab336ee34aa2f6a6d7ba35b376ebe
Holly Hosford <61231286+Holly-Hosford@users.noreply.github.com>
2021-12-05 20:14:35 +0000

    Create variables and constructor

--- Java_Assignment3/src/main/java/com/example/java_assignment3/CheckRequirements.java
+++ Java_Assignment3/src/main/java/com/example/java_assignment3/CheckRequirements.java
@@ -1,4 +1,12 @@
 package com.example.java_assignment3;
 
 public class CheckRequirements {
+  private final PasswordField passwordText;
+  private final TextField emailText;
+  
+  public CheckRequirements(PasswordField passwordText, TextField emailText) {
+        this.passwordText = passwordText;
+        this.emailText = emailText;
+    }
+  
 }
