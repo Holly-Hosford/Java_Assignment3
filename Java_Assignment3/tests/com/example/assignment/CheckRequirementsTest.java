7e1e21782b2079ba2cdf55dc489992763f291eb9
Holly Hosford <61231286+Holly-Hosford@users.noreply.github.com>
2021-12-05 22:48:28 +0000

    Original Fail in TDD
    Trouble uploading in git inIntellij so had to upload on website

--- Java_Assignment3/tests/com/example/assignment/CheckRequirementsTest.java
+++ Java_Assignment3/tests/com/example/assignment/CheckRequirementsTest.java
@@ -3,5 +3,44 @@ package com.example.assignment;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CheckRequirementsTest {
+      package com.example.assignment;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheckRequirementsTest {
+    private final String password  = "";
+    private final String email = "";
+
+    @Test
+    void testPasswordLength() {
+        CheckRequirements test = new CheckRequirements(password, email);
+        assertEquals(true, test.passwordLength(password));
+
+    }
+
+    @Test
+    void testPasswordOneLetter() {
+        CheckRequirements test = new CheckRequirements(password, email);
+        assertEquals(true, test.oneLetter(password));
+    }
+
+    @Test
+    void testPasswordOneDigit() {
+        CheckRequirements test = new CheckRequirements(password, email);
+        assertEquals(true, test.oneDigit(password));
+    }
+
+    @Test
+    void testPasswordOneSymbol() {
+        CheckRequirements test = new CheckRequirements(password, email);
+        assertEquals(true, test.oneSymbol(password));
+    }
+
+    @Test
+    void testEmailFormat() {
+        CheckRequirements test = new CheckRequirements(password, email);
+        assertEquals(true, test.emailCorrect(email));
+    }
 }
