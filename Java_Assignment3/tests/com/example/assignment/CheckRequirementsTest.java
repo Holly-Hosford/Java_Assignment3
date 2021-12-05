5d254b91de1e8d7c31ff6856f5e3fe5f4dc78492
Holly Hosford <61231286+Holly-Hosford@users.noreply.github.com>
2021-12-05 22:57:02 +0000

    Minimum working test

--- Java_Assignment3/tests/com/example/assignment/CheckRequirementsTest.java
+++ Java_Assignment3/tests/com/example/assignment/CheckRequirementsTest.java
@@ -5,37 +5,37 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CheckRequirementsTest {
-    private final String password  = "1a@sdlfbhosbf";
-    private final String email = "abc@gmail.com";
+    private final String password  = "";
+    private final String email = "";
 
     @Test
     void testPasswordLength() {
-        CheckRequirements test = new CheckRequirements(password, email);
-        assertEquals(true, test.passwordLength(password));
+        String password = "1a@sdlfbhosbf";
+        assertEquals(true, CheckRequirements.passwordLength(password));
 
     }
 
     @Test
     void testPasswordOneLetter() {
-        CheckRequirements test = new CheckRequirements(password, email);
-        assertEquals(true, test.oneLetter(password));
+        String password = "1a@sdlfbhosbf";
+        assertEquals(true, CheckRequirements.oneLetter(password));
     }
 
     @Test
     void testPasswordOneDigit() {
-        CheckRequirements test = new CheckRequirements(password, email);
-        assertEquals(true, test.oneDigit(password));
+        String password = "1a@sdlfbhosbf";
+        assertEquals(true, CheckRequirements.oneDigit(password));
     }
 
     @Test
     void testPasswordOneSymbol() {
-        CheckRequirements test = new CheckRequirements(password, email);
-        assertEquals(true, test.oneSymbol(password));
+        String password = "1a@sdlfbhosbf";
+        assertEquals(true, CheckRequirements.oneSymbol(password));
     }
 
     @Test
     void testEmailFormat() {
-        CheckRequirements test = new CheckRequirements(password, email);
-        assertEquals(true, test.emailCorrect(email));
+        String email = "abc@gmail.com";
+        assertEquals(true, CheckRequirements.emailCorrect(email));
     }
 }
