7fc32ab08b1321ac9b70410c023e4b7ca1545cce
Holly Hosford <61231286+Holly-Hosford@users.noreply.github.com>
2021-12-05 18:47:30 +0000

    Basic files needed for javafx project
    Includes sample app Intellij created

---
+++ Java_Assignment3/src/main/java/com/example/java_assignment3/CheckRequirements.java
@@ -0,0 +1,4 @@
+package com.example.java_assignment3;
+
+public class CheckRequirements {
+}
---
+++ Java_Assignment3/tests/com/example/assignment/CheckRequirementsTest.java
@@ -0,0 +1,7 @@
+package com.example.assignment;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheckRequirementsTest {
+
+}
